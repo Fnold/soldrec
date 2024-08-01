fa3fd3bd19eec43bbc8c884f31ae62e2c912fae7
Fnold <swedishmedic123@gmail.com>
2024-08-01 23:54:22 +0200

    clear search button, search by artists

--- js/card-search.js
+++ js/card-search.js
@@ -1,14 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const cards = document.querySelectorAll('.card');
-    document.querySelector('input').addEventListener('input', () => {
-        const search_words = document.querySelector('input').value.trim().toLowerCase().split(/\s+/);
-        cards.forEach(card => {
-            const spanText = card.querySelector('span').textContent.toLowerCase();
-            if (search_words.every(word => spanText.includes(word))) {
-                card.style.display = "block";
-            } else {
-                card.style.display = "none";
-            }
-        });
-    });
+document.getElementById('search_clear').style.display = "none";
+
+document.querySelector('input').addEventListener('input', () => {
+    document.getElementById('search_clear').style.display = "block";
 });
+
+function search() {
+    const cards = document.querySelectorAll('.card');
+    const search_words = document.querySelector('input').value.trim().toLowerCase().split(/\s+/);
+    cards.forEach(card => {
+        const spanText = card.querySelector('span').textContent.toLowerCase();
+        if (search_words.every(word => spanText.includes(word))) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+});
+}
