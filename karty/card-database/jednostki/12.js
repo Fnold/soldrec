bada5539e2022e7aa1e7a47199517ffe70d2ad95
Fnold <swedishmedic123@gmail.com>
2024-08-26 22:40:18 +0200

    change cards from png to webp, add metrics charts, small CSS fixes

--- karty/card-database/jednostki/12.js
+++ karty/card-database/jednostki/12.js
@@ -24,8 +24,141 @@ function latest() {
     document.getElementById("card_subtype").innerHTML = card_subtype;
     document.getElementById("card_stats").innerHTML = card_stats;
     document.getElementById("card_author").innerHTML = card_author;
-    document.getElementById("card_image").src = "../../assets/cards/latest/jednostki/12.png";
+    document.getElementById("card_image").src = "../../assets/cards/latest/jednostki/12.webp";
     document.getElementById("card_image").alt = card_title;
     document.getElementById("latest_selector").innerHTML = `&gt;<u>Najnowsza</u>`;
 }
 document.addEventListener("DOMContentLoaded", latest);
+
+    const ctxP = document.getElementById('chart_popularity');
+    const ctxD = document.getElementById('chart_delta');
+    const current_popularity = 50;
+    const current_delta = 0;
+    document.getElementById("popularity").innerHTML = "&nbsp;"+current_popularity+"%&nbsp;";
+    document.getElementById("delta").innerHTML = "&nbsp;"+current_delta+"&nbsp;";
+    
+    function interpolateColor(color1, color2, factor) {
+        if (arguments.length < 3) { 
+            factor = 0.5; 
+        }
+        const result = color1.slice();
+        for (let i = 0; i < 3; i++) {
+            result[i] = Math.round(result[i] + factor * (color2[i] - color1[i]));
+        }
+        return result;
+    }
+
+    function getColorPopularity(value) {
+        const red = [255, 0, 0];
+        const orange = [255, 127, 0];
+        const yellow = [240, 240, 0];
+        const green = [0, 192, 0];
+        let Pcolor;
+        if (value <= 30) {
+            Pcolor = interpolateColor(red, orange, value / 30);
+        } else if (value <= 60) {
+            Pcolor = interpolateColor(orange, yellow, (value - 30) / 30);
+        } else if (value <= 90) {
+            Pcolor = interpolateColor(yellow, green, (value - 60) / 30);
+        } else {
+            Pcolor = green;
+        }
+        return 'rgb(' + Pcolor.join(',') + ')';
+    }
+
+    function getColorDelta(value) {
+        const red = [255, 0, 0];
+        const orange = [255, 127, 0];
+        const yellow = [240, 240, 0];
+        const green = [0, 192, 0];
+        let Dcolor;
+        if (value <= -4.5) {
+            Dcolor = interpolateColor(red, orange, (value + 10) / 5);
+        } else if (value <= 4.5) {
+            Dcolor = interpolateColor(orange, yellow, (value + 5) / 10);
+        } else {
+            Dcolor = interpolateColor(yellow, green, (value - 5) / 5);
+        }
+        return 'rgb(' + Dcolor.join(',') + ')';
+    }
+
+    const Pcolor = getColorPopularity(current_popularity);
+    const Dcolor = getColorDelta(current_delta);
+    document.getElementById('popularity').style.backgroundColor = Pcolor;
+    document.getElementById('delta').style.backgroundColor = Dcolor;
+
+    new Chart(ctxP, {
+        type: 'line',
+        data: {
+        labels: ['1.2.1'],
+        datasets: [{
+            label: 'Frekwencja',
+            data: [current_popularity],
+            borderWidth: 1,
+            borderColor: '#FFC000',
+            backgroundColor: '#FFC000'
+        }]
+        },
+        options: {
+        pointRadius: 5,
+        pointStyle: 'rectRot',
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+        scales: {
+            y: {
+            max: 100,
+            beginAtZero: true,
+            grid: {
+                color: '#404040'
+            },
+            },
+            x: {
+            grid: {
+                color: '#505050'
+            }
+            }
+        }
+        }
+    });
+    new Chart(ctxD, {
+        type: 'line',
+        data: {
+        labels: ['1.2.1'],
+        datasets: [{
+            label: 'Delta',
+            data: [current_delta],
+            borderWidth: 1,
+            borderColor: '#0094FF',
+            backgroundColor: '#0094FF'
+        }]
+        },
+        options: {
+        pointRadius: 5,
+        pointStyle: 'rectRot',
+        plugins: {
+            legend: {
+                display: false,
+            },
+        },
+        scales: {
+            y: {
+            max: 10,
+            min: -10,
+            grid: {
+                color: '#404040'
+            },
+            ticks: {
+                stepSize: 1
+            }
+            },
+            x: {
+            grid: {
+                color: '#505050'
+            }
+            }
+        }
+        }
+    });
