1a49c951e877f5c0fc18e9a9cb3cc357ad2c4e92
Fnold <swedishmedic123@gmail.com>
2025-12-22 00:41:48 +0100

    add old "Hoplita" version to database

--- karty/card-database/jednostki/52.js
+++ karty/card-database/jednostki/52.js
@@ -1,6 +1,42 @@
 function latest() {
     const card_title = "Hoplita";
     const site_title = card_title;
+    const card_cost = "5J";
+    const card_cost_info = "≥5 Wystawione Jednostki";
+    const card_type = "Jednostka - Operator bez Gwiazdki";
+    const card_subtype = `
+        <p>
+        Człowiek
+        </p>`;
+    const card_description = `
+        <p>Pokaż górną kartę twojej talii. Jeżeli jest to jednostka, dobierz te kartę.</p>
+        <br>
+        <p>Za każdym razem, gdy ta jednostka atakuje, zyskuje ona +X/+0/+0/+0, gdzie X jest liczbą atakujących jednostek.</p>`;
+    const card_stats = `
+        <p>
+        3/2/3/3
+        </p>`;
+    const card_author = `
+        <i>
+        Ilustracja: Fnold 2021
+        </i>`;
+    document.getElementById("card_title").innerHTML = card_title;
+    document.getElementById("site_title").innerHTML = site_title;
+    document.getElementById("card_cost").innerHTML = card_cost;
+    document.getElementById("card_cost_info").title = card_cost_info;
+    document.getElementById("card_type").innerHTML = card_type;
+    document.getElementById("card_subtype").innerHTML = card_subtype;
+    document.getElementById("card_description").innerHTML = card_description;
+    document.getElementById("card_stats").innerHTML = card_stats;
+    document.getElementById("card_author").innerHTML = card_author;
+    document.getElementById("card_image").src = "../../assets/cards/latest/jednostki/52.webp";
+    document.getElementById("card_image").alt = card_title;
+    document.getElementById("latest_selector").innerHTML = `&gt;<u>Najnowsza</u>`;
+    document.getElementById("alpha_selector").innerHTML = `<u>Alpha</u>`;
+}
+function alpha() {
+    const card_title = "Hoplita";
+    const site_title = card_title;
     const card_cost = "8M 8P";
     const card_cost_info = "8 Many i 8 Popularności";
     const card_type = "Jednostka - Operator z Gwiazdką";
@@ -29,9 +65,10 @@ function latest() {
     document.getElementById("card_description").innerHTML = card_description;
     document.getElementById("card_stats").innerHTML = card_stats;
     document.getElementById("card_author").innerHTML = card_author;
-    document.getElementById("card_image").src = "../../assets/cards/latest/jednostki/52.webp";
+    document.getElementById("card_image").src = "../../assets/cards/alpha/jednostki/52.webp";
     document.getElementById("card_image").alt = card_title;
-    document.getElementById("latest_selector").innerHTML = `&gt;<u>Najnowsza</u>`;
+    document.getElementById("latest_selector").innerHTML = `<u>Najnowsza</u>`;
+    document.getElementById("alpha_selector").innerHTML = `&gt;<u>Alpha</u>`;
 }
 document.addEventListener("DOMContentLoaded", latest);
 
